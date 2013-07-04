efaff7d1b12c379a18c7fa164379493da9640145
Elliott <erchenger@gmail.com>
2013-07-04 09:43:11 -0500

    Adding the main class that will run all of the data structures as well as my ignore file.

--- Data_Structures/src/main/DataStructures.java
+++ Data_Structures/src/main/DataStructures.java
@@ -2,4 +2,8 @@ package main;
 
 public class DataStructures {
 
+	public static void main (String args[]){
+		//This is where the entire project will run and allow you to try different data structures
+
+	}
 }
